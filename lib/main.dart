0a58ffb17a4e5ba031d5d1887fa505e93e31bae9
johannkarlsson <johannk20@ru.is>
2022-07-27 14:16:02 +0000

    init

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My first app'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+          'Body text',
+          style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+      )
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(onPressed: () {},
+      child: Text('click'),
+      backgroundColor: Colors.red[600],
+    ),
+  ),
+));
+
